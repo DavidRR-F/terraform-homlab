f4824b715fc1a004b763b5804c098900c08a37a6
DavidRR-F <david.rosefranklin96@gmail.com>
2024-11-23 16:40:43 -0500

    blob storage url to env

--- backend.tf
+++ backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "s3" {
     bucket                      = "tfstate"
     key                         = "terraform.tfstate"
-    endpoint                    = "https://minio.home.shagbaggins.dev"
     skip_credentials_validation = true
     skip_requesting_account_id  = true
     skip_metadata_api_check     = true
